6839fd7a1af3732f54ed2360561f2712618e3c26
gabyrubgue <gabrielarubioguevara@gmail.com>
2026-05-09 13:10:57 -0500

    cambios finales

--- componentes.js
+++ componentes.js
@@ -9,3 +9,31 @@ fetch("footer.html")
   .then(data => {
     document.getElementById("footer").innerHTML = data;
   });
+
+
+
+  fetch("header.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+
+    // ACTIVAR LINK ACTUAL
+    const enlaces = document.querySelectorAll(".nav a");
+
+    const paginaActual =
+      window.location.pathname.split("/").pop();
+
+    enlaces.forEach(enlace => {
+      const href = enlace.getAttribute("href");
+
+      if (href === paginaActual) {
+        enlace.classList.add("activo");
+      }
+    });
+  });
+
+fetch("footer.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  });
